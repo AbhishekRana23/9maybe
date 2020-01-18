ec1a06c19cf962755a8c259e2b2a0b787d45f2c0
Abhishek Rana <40822125+AbhishekRana23@users.noreply.github.com>
2020-01-18 13:31:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-apt update && apt install -y git whiptail && %cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "b1d1d962f0eca086bf15108eeb1c691e" "30" "80" "6" && rm /root/9Hits/install.sh
+apt update && apt install -y git whiptail && cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "b1d1d962f0eca086bf15108eeb1c691e" "30" "80" "6" && rm /root/9Hits/install.sh
